8d43250f0520565376a9d274df077722d08c85e7
Yordy2001 <kingyordi2001@gmail.com>
2025-02-04 14:22:37 -0400

    feat create initial folders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ClientsComponent } from './dashboard/clients/clients.component';
+import { AppointmentsComponent } from './dashboard/appointments/appointments.component';
+import { FinancialComponent } from './dashboard/financial/financial.component';
+import { InventoryComponent } from './dashboard/inventory/inventory.component';
+import { ServicesComponent } from './dashboard/services/services.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'dashboard', children: [
+        { path: 'appointments', component: AppointmentsComponent },
+        { path: 'clients', component: ClientsComponent },
+        { path: 'services', component: ServicesComponent },
+        { path: 'inventory', component: InventoryComponent },
+        { path: 'financial', component: FinancialComponent },
+    ]},
+];
---
+++ src/app/dashboard/financial/financial.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-financial',
+  imports: [],
+  templateUrl: './financial.component.html',
+  styleUrl: './financial.component.css'
+})
+export class FinancialComponent {
+
+}
