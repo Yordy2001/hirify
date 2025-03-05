650241af052aaf80d0d2ba12fde6917bc9533e0a
Yordy2001 <kingyordi2001@gmail.com>
2025-03-04 21:40:17 -0400

    feat create add button component to open modal in each page

--- src/app/pages/clients/clients.component.ts
+++ src/app/pages/clients/clients.component.ts
@@ -3,14 +3,21 @@ import { ClientsService } from './clients.service';
 import { Client } from './models/client.model';
 import { CommonModule } from '@angular/common';
 
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { AddButtonComponent } from "../../shared/components/buttons/add-button/add-button.component";
+
 @Component({
   selector: 'app-clients',
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatInputModule, MatFormFieldModule, MatButtonModule, AddButtonComponent],
   templateUrl: './clients.component.html',
   styleUrl: './clients.component.css'
 })
 export class ClientsComponent implements OnInit {
-
+  page = 'Cliente'
+  displayedColumns: string[] = ['name', 'whatsapp', 'gender'];
   clients: Client[] = []
 
   constructor(
@@ -20,13 +27,19 @@ export class ClientsComponent implements OnInit {
   ngOnInit() {
     this.clientsService.get().subscribe(
       (clients: Client[]) => {
+        console.log(clients);
+        
         this.clients.push(...clients)
       },
     );
+  }
+  applyFilter(event: Event) {
+    // const filterValue = (event.target as HTMLInputElement).value;
+    // this.clients.filter = filterValue.trim().toLowerCase();
   }
 
   // !Todo : Add fill table
   // !Todo : NgOnChanges
 
-  
+
 }
---
+++ src/app/shared/components/buttons/add-button/add-button.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-add-button',
+  imports: [MatButtonModule, MatIconModule],
+  templateUrl: './add-button.component.html',
+})
+export class AddButtonComponent {
+  @Input() page:string = '';
+}
